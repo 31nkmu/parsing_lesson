0424c3a34bfdd0297e6961cc41be2f07aa09c62a
31nkmu <karimovbillal20002@gmail.com>
2022-10-13 21:29:30 +0600

    added pages and try-except

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import csv
+BASE_URL = 'https://svetofor.info/sotovye-telefony-i-aksessuary/vse-smartfony/smartfony-s-podderzhkoy-4g-ru'
 
 
 def get_html(url):
@@ -32,23 +33,37 @@ def get_data(soup):
         write_csv({'title': title, 'img': img, 'price': price})
 
 
-def get_page(soup):
-    page = html.find('a', class_='cm-history ty-pagination__item hidden-phone ty-pagination__range cm-ajax cm-ajax-full-render')
+def get_page():
+    html = get_html(BASE_URL)
+    soup = get_soup(html)
+    page = soup.find('a', class_='cm-history ty-pagination__item hidden-phone ty-pagination__range cm-ajax cm-ajax-full-render').text
+    page = page.split('-')
+    return int(page[-1])
 
 
 def write_csv(data):
     with open('phones.csv', 'a') as file:
         names = ['title', 'img', 'price']
-        writer = csv.DictWriter(file, delimitr=',', fieldnames=names)
+        writer = csv.DictWriter(file, delimiter=',', fieldnames=names)
         writer.writerow(data)
 
 
 def main():
-    BASE_URL = 'https://svetofor.info/sotovye-telefony-i-aksessuary/vse-smartfony/smartfony-s-podderzhkoy-4g-ru'
-    html = get_html(BASE_URL)
-    soup = get_soup(html)
-    page = get_page(soup)
-    get_data(soup)
+    # BASE_URL = 'https://svetofor.info/sotovye-telefony-i-aksessuary/vse-smartfony/smartfony-s-podderzhkoy-4g-ru'
+    # html = get_html(BASE_URL)
+    # soup = get_soup(html)
+    # page = get_page(soup)
+    # get_data(soup)
+    # print(1)
+    try:
+        for i in range(1, get_page()+1):
+            url = f'{BASE_URL}/page-{i}/'
+            html = get_html(url)
+            soup = get_soup(html)
+            get_data(soup)
+            print(i)
+    except AttributeError:
+        print('ошибка')
 
 
 if __name__ == '__main__':
